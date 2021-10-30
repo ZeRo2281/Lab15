62f68684d0f1b6b2433a74543748525148e0a973
Данил Березенцев <danil.ber@mail.ru>
2021-10-30 14:22:39 +0300

    Add LaterInYear function

--- main.cpp
+++ main.cpp
@@ -9,7 +9,15 @@ bool IsLeapYear(int y)
 	return (y % 4 == 0) && (y % 100 != 0) || (y % 400 == 0);
 }
 
-int SecondsInMinute(int m)
+(int,int) LaterInYear(int d1, int m1, int d2, int m2)
 {
-	return m * 60;
+	if (m1 > m2)
+		return (d1, m1);
+	else if (m2 > m1)
+		return (d2, m2);
+	else if (d1 > d2)
+		return (d1, m1);
+	else if (d2 > d1)
+		return (d2, m2)
+	else return (-1, -1);
 }
